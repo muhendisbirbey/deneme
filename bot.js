a39a7e40e3e1f3ef6bc8fcf3580ec304ce12707c
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2019-04-14 00:51:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client ();
 
 
 bot.on('ready', () => {
-  bot.user.setGame('instagram : pubgmuhendisi') })
+  bot.user.setGame('instagram : mühendisbeyilemühendishanım') })
 
 
 
